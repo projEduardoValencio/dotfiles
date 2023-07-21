a229ebdea36937b308bcea0bd7512872db7c2785
projEduardoValencio <eduardo.valencio@gmail.com>
2023-07-20 21:21:25 -0300

    feat: dmenu-dofiles script

---
+++ Scripts/dmenu-dotfiles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TERMINAL="alacritty"
+
+# Definindo editor
+EDITOR="nvim"
+
+declare -a dotfiles=(
+    "i3 - $HOME/.config/i3/config"
+    "nvim - $HOME/.config/nvim/init.lua"
+    "ranger - $HOME/.config/ranger/rc.conf"
+)
+
+# Selecionando o arquivo de configuração
+opt=$(printf '%s\n' "${dotfiles[@]}" | dmenu -i -l 5 -p 'Dotfiles  ')
+
+# Formatando com aws para pegar o valor da ultima coluna
+file_path=$(printf '%s\n' "$opt" | awk '{print $NF}')
+
+# Abrindo arquivo para edição
+alacritty -e $EDITOR $file_path 
