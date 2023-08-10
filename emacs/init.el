20beee1c0cf53df39aeec7847d06590cda439092
projEduardoValencio <eduardo.valencio.santos@gmail.com>
2023-08-10 20:30:33 -0300

    feat: change colors for company

--- emacs/init.el
+++ emacs/init.el
@@ -343,8 +343,12 @@ show-paren-style
     :init (global-company-mode)
     :config
 	(setq company-minimum-prefix-length 2)
+	;; Be careful - Change color of company pop up
+	(custom-set-faces
+	 '(company-tooltip ((t (:background "#111" :foreground "#fff"))))
+	 '(company-tooltip-selection ((t (:background "#444")))))
     :bind
-	("C-<return>" . company-complete))
+    ("C-<return>" . company-complete))
 
 ;; treemacs file explorer
 (use-package treemacs
@@ -378,3 +382,58 @@ show-paren-style
 ;;(setq browse-url-browser-function 'eww-browse-url)
 (setq browse-url-browser-function 'w3m-browse-url)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#000000" "#ff6c60" "#A8FF60" "#FFFFB6" "#96CBFE" "#FF73FD" "#C6C5FE" "#f6f3e8"])
+ '(custom-safe-themes
+   '("1aa4243143f6c9f2a51ff173221f4fd23a1719f4194df6cef8878e75d349613d" default))
+ '(evil-mode t)
+ '(exwm-floating-border-color "#5B6268")
+ '(fci-rule-color "#5B6268")
+ '(highlight-tail-colors
+   ((("#101909" "#A8FF60" "green")
+     . 0)
+    (("#131319" "#C6C5FE" "brightcyan")
+     . 20)))
+ '(ispell-dictionary nil)
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#96CBFE"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#A8FF60"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
+ '(objed-cursor-color "#ff6c60")
+ '(package-selected-packages
+   '(cmake-mode yasnippet-snippets which-key w3m vterm use-package-chords undo-tree try treemacs-tab-bar sly rainbow-delimiters quickrun projectile org-super-agenda org-habit-stats org-drill org-bullets neotree magit lsp-ui lsp-java ivy-rich helm-swoop helm-lsp helm-descbinds general flycheck exec-path-from-shell evil-org evil-collection eglot doom-themes counsel company centaur-tabs auto-complete all-the-icons))
+ '(pdf-view-midnight-colors (cons "#f6f3e8" "#000000"))
+ '(rustic-ansi-faces
+   ["#000000" "#ff6c60" "#A8FF60" "#FFFFB6" "#96CBFE" "#FF73FD" "#C6C5FE" "#f6f3e8"])
+ '(vc-annotate-background "#000000")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#A8FF60")
+    (cons 40 "#c5ff7c")
+    (cons 60 "#e2ff99")
+    (cons 80 "#FFFFB6")
+    (cons 100 "#f7ea9a")
+    (cons 120 "#f0d57e")
+    (cons 140 "#E9C062")
+    (cons 160 "#f0a695")
+    (cons 180 "#f78cc9")
+    (cons 200 "#FF73FD")
+    (cons 220 "#ff70c8")
+    (cons 240 "#ff6e94")
+    (cons 260 "#ff6c60")
+    (cons 280 "#cf615a")
+    (cons 300 "#9f5855")
+    (cons 320 "#6f4e4f")
+    (cons 340 "#5B6268")
+    (cons 360 "#5B6268")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
