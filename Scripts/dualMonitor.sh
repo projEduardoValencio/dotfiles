f5edb55c14f5636baf22df8bb3a3ed413c22877c
Eduardo Valencio <eduardo.valencio.santos@gmail.com>
2023-12-27 14:29:43 -0300

    feat: rofi for emojis and dmenu

---
+++ Scripts/dualMonitor.sh
@@ -0,0 +1,1 @@
+xrandr --auto --output HDMI-1-3 --mode 1920x1080 --rate 120 --left-of eDP-1
---
+++ Scripts/rofi-dotfiles.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+TERMINAL="alacritty"
+
+# Definindo editor
+EDITOR="nvim"
+
+declare -a dotfiles=(
+    "i3 - $HOME/.config/i3/config"
+    "alacritty - $HOME/.config/alacritty/alacritty.yml"
+    "nvim - $HOME/.config/nvim/init.lua"
+    "emacs - $HOME/.config/emacs/config.org"
+    "ranger - $HOME/.config/ranger/rc.conf"
+)
+
+# Selecionando o arquivo de configuração
+opt=$(printf '%s\n' "${dotfiles[@]}" | rofi -dmenu -i -l 5 -p 'Dotfiles  ')
+
+# Formatando com aws para pegar o valor da ultima coluna
+file_path=$(printf '%s\n' "$opt" | awk '{print $NF}')
+
+# Abrindo arquivo para edição
+alacritty -e $EDITOR $file_path 
