0ae54cd188cfc8ace39aa9edc97ac03122528eed
Eduardo Valencio <eduardo.valencio.santos@gmail.com>
2024-02-05 11:42:44 -0300

    feat: fish shell

---
+++ terminal/fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -Ux fish_user_paths /home/eduardo/.cargo/bin $fish_user_paths
+
+set -Ux JAVA_HOME /usr/local/java/jdk1.8.0_202/bin
+set -Ux JRE_HOME /usr/local/java/jdk1.8.0_202/jre/bin
+set -Ux CLASSPATH /usr/local/java/jdk1.8.0_202/lib
+set -Ux PATH $PATH:$HOME/bin:$JAVA_HOME:$JRE_HOME:$CLASSPATH
+
+set -x _PENTAHO_JAVA $JRE_HOME
+
+
+set -Ux STARSHIP_CONFIG /home/eduardo/.config/starship/starship.toml
+starship init fish | source
+
+neofetch
