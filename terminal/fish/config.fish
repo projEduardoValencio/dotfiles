40fe9bc14402424e95a6dc27c5dbfcfcdc75a071
Eduardo Valencio <eduardo.valencio.santos@gmail.com>
2024-02-07 19:45:37 -0300

    feat: lsp nvim

--- terminal/fish/config.fish
+++ terminal/fish/config.fish
@@ -4,9 +4,10 @@ end
 
 set -Ux fish_user_paths /home/eduardo/.cargo/bin $fish_user_paths
 
-set -Ux JAVA_HOME /usr/local/java/jdk1.8.0_202/bin
+set -Ux JAVA_HOME /usr/lib/jvm/java-11-openjdk-amd64
 set -Ux JRE_HOME /usr/local/java/jdk1.8.0_202/jre/bin
 set -Ux CLASSPATH /usr/local/java/jdk1.8.0_202/lib
+
 set -Ux PATH $PATH:$HOME/bin:$JAVA_HOME:$JRE_HOME:$CLASSPATH
 
 set -x _PENTAHO_JAVA $JRE_HOME
