651ca567c8401a05b403d0c4882e593d781b3a81
eduardoValencio <eduardo.santos@supportweb.com.br>
2024-02-09 10:53:27 -0300

    feat: treesitter

--- terminal/nvim/lua/eduardo/plugins/lsp/lspconfig.lua
+++ terminal/nvim/lua/eduardo/plugins/lsp/lspconfig.lua
@@ -94,6 +94,11 @@ return {
       on_attach = on_attach,
     })
 
+    lspconfig["vuels"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+
     -- configure lua server (with special settings)
     lspconfig["lua_ls"].setup({
       capabilities = capabilities,
